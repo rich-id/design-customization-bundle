50e142d235ef66d9be7ba40dc5f0eb875abde4c2
Matthias Devlamynck <matthias.devlamynck@rich-id.fr>
2024-05-02 17:01:47 +0200

    Remove deleted doctrine type from migrations

--- src/Infrastructure/Migrations/Version20210726125431.php
+++ src/Infrastructure/Migrations/Version20210726125431.php
@@ -13,7 +13,7 @@ class Version20210726125431 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE module_design_customization_configuration (id INT UNSIGNED AUTO_INCREMENT NOT NULL, slug VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, type ENUM(\'font\', \'radius\', \'color\', \'image\') NOT NULL COMMENT \'(DC2Type:DesignConfigurationType)\', `position` INT UNSIGNED NOT NULL, value VARCHAR(600) DEFAULT NULL, default_value VARCHAR(600) NOT NULL, date_update DATETIME NOT NULL, UNIQUE INDEX UNIQ_D4B1559D989D9B62 (slug), UNIQUE INDEX UNIQ_D4B1559D5E237E06 (name), UNIQUE INDEX design_configuration_type_position_UNIQUE (type, `position`), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE module_design_customization_configuration (id INT UNSIGNED AUTO_INCREMENT NOT NULL, slug VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, type ENUM(\'font\', \'radius\', \'color\', \'image\') NOT NULL, `position` INT UNSIGNED NOT NULL, value VARCHAR(600) DEFAULT NULL, default_value VARCHAR(600) NOT NULL, date_update DATETIME NOT NULL, UNIQUE INDEX UNIQ_D4B1559D989D9B62 (slug), UNIQUE INDEX UNIQ_D4B1559D5E237E06 (name), UNIQUE INDEX design_configuration_type_position_UNIQUE (type, `position`), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
